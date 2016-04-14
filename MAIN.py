6e98b5e437a8077ce0fc4182cf5e13486409ad7a
keechow <keechow@gmail.com>
2016-04-14 16:47:14 +0800

    separate result according to months for 4d, 5d, 6d

--- MAIN.py
+++ MAIN.py
@@ -1,15 +1,21 @@
 __author__ = 'keechow'
 
+from file_handler import read4d_result
 from file_handler import read5d_result
-from separate_by_month import separate_by_month
+from file_handler import read6d_result
+from separate_by_month import separate_4d
+from separate_by_month import separate_5d
+from separate_by_month import separate_6d
 
+data_4d = read4d_result("4D.txt")
 data_5d = read5d_result("5D.txt")
-result_by_month = separate_by_month(data_5d)
+data_6d = read6d_result("6D.txt")
 
-jan = result_by_month[0]
-print len(jan)
-print jan
+data_4d_by_month = separate_4d(data_4d)
+data_5d_by_month = separate_5d(data_5d)
+data_6d_by_month = separate_6d(data_6d)
 
+#print result6d_by_month[0]
 
 print "\n\n\n============== SUCCESSFULLY EXECUTED ================"
 print "============== SUCCESSFULLY EXECUTED ================"
