16b404b3a5df92d483833f2866ef47d431aa2366
keechow <keechow@gmail.com>
2016-06-10 11:07:40 +0800

    plot numrange vs drawnum using matplotlib

---
+++ numrange_drawnum_plot.py
@@ -0,0 +1,19 @@
+__author__ = 'keechow'
+import matplotlib.pyplot as plt
+from file_handler import read5d_result_w_draw_no as read5d
+
+x_elements = []
+y_elements=[]
+
+data = read5d("5D.txt")
+
+for each_set in data[-30:]:
+	prize1 = each_set[2]
+	prize1_numcat = int(prize1[0])
+	y_elements.append(prize1_numcat)
+	x_elements.append(int(each_set[0]))
+
+print x_elements
+
+plt.plot(x_elements,y_elements, "rs")
+plt.show()
