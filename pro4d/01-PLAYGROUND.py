ea9e1ab43085a43cffb645a885e623becc6f8964
keechow <keechow@gmail.com>
2016-06-17 16:46:37 +0800

    match 1,2,3 num drawn in the past with recent 1,2,3 num

--- pro4d/01-PLAYGROUND.py
+++ pro4d/01-PLAYGROUND.py
@@ -16,7 +16,7 @@ for each_draw in result:
 	p1 = each_draw[2]
 	p2 = each_draw[3]
 	p3 = each_draw[4]
-	if year == "13" or year == "14" or year == "15" or year == "16":
+	if year == "09" or year == "10" or year == "11" or year == "12" or year == "08":
 		prize1.append(p1)
 		prize2.append(p2)
 		prize3.append(p3)
@@ -25,3 +25,18 @@ print len(prize1)
 print len(prize2)
 print len(prize3)
 
+latest_3prize = []
+for each_draw in latest_result:
+    latest_3prize.append(each_draw[2])
+    latest_3prize.append(each_draw[3])
+    latest_3prize.append(each_draw[4])
+
+match1 = set(prize1) & set(latest_3prize)
+match2 = set(prize2) & set(latest_3prize)
+match3 = set(prize3) & set(latest_3prize)
+
+print match1
+print
+print match2
+print
+print match3
